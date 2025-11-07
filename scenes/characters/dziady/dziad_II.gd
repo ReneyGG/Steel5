6020ede392005447bef03c25f2c308f7965f54f6
Bartłomiej Kasprzyk <dolarbartek@wp.pl>
2025-11-07 21:11:39 +0100

    new skins

--- scenes/characters/dziady/dziad_II.gd
+++ scenes/characters/dziady/dziad_II.gd
@@ -1,1 +1,8 @@
 extends "res://scenes/characters/dziady/dziad.gd"
+
+func handle_animation():
+	if abs(velocity) > Vector2.ZERO:
+		model_3d.look_at(Vector3(-velocity.x, 0, -velocity.y).rotated(Vector3(0,1,0), deg_to_rad(45)))
+		model_3d.walk()
+	else:
+		model_3d.idle()
