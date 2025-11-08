a12b69a2e3346123ceb6fb7a018dde23d8bb9286
Bartłomiej Kasprzyk <dolarbartek@wp.pl>
2025-11-08 22:20:58 +0100

    avatars polish

--- scenes/menu/avatars/avatars.gd
+++ scenes/menu/avatars/avatars.gd
@@ -4,13 +4,19 @@ var dziad_I : DZIAD
 var dziad_II : DZIAD
 @onready var dziad_i_avatar: TextureRect = $Control/Dziad_I_Avatar
 @onready var dziad_ii_avatar: TextureRect = $Control/Dziad_II_Avatar
+@onready var dziad_i_avatar_alt: TextureRect = $Control/Dziad_I_Avatar_Alt
 
-#func _ready() -> void:
-	#var dziady = get_tree().get_nodes_in_group("dziady")
-	#dziad_I = dziady[0]
-	#dziad_II = dziady[1]
-	#dziad_II.on_merge.connect(on_dziad_merge)
-	#
-#func on_dziad_merge():
-	#pass
-	#dziad_ii_avatar.texture
+func _ready() -> void:
+	var dziady = get_tree().get_nodes_in_group("dziady")
+	dziad_I = dziady[0]
+	dziad_II = dziady[1]
+	dziad_II.on_merge.connect(on_dziad_merge)
+	dziad_II.on_end_merge.connect(on_dziad_end_merge)
+	
+func on_dziad_merge():
+	dziad_ii_avatar.visible = false
+	dziad_i_avatar_alt.visible = true
+	
+func on_dziad_end_merge():
+	dziad_ii_avatar.visible = true
+	dziad_i_avatar_alt.visible = false
