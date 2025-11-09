142996a1438790f1b41b153936ac8e1df4911f14
ReneyGG <jakubczyzycki12@gmail.com>
2025-11-09 17:49:08 +0100

    pushpush

--- scenes/menu/main/main.gd
+++ scenes/menu/main/main.gd
@@ -1,11 +1,16 @@
 extends Node2D
 
 var t = 0
-var ts = ["LEPIEJ NA PADZIE","GRA DLA DWÓCH","DZIADÓW DWÓCH"]
+var ts = ["Podziemia Las Vegas to miejsce dla tych, którzy postawili wszystko na jedną kartę. Życie mija tutaj od powodzi do powodzi.","Gra przeznaczona dla dwóch graczy.","DZIADÓW DWÓCH"]
 
 var start := false
 
+func _ready():
+	VegasMusic.play()
+
 func _on_button_start_pressed():
+	VegasMusic.stop()
+	FightclubMusic.play()
 	start = true
 	$Timer.start()
 
@@ -16,14 +21,16 @@ func _physics_process(_delta):
 	if start:
 		$Camera2D.position.y += 10
 
-func _on_timer_timeout():
+func _on_timer_timeout():	
 	$CanvasLayer/Control/ButtonOk.show()
 	$CanvasLayer/Control/Label.text = ts[t]
+	if t == 2:
+		$CanvasLayer/Control/Sewer.show()
 
 func _on_button_ok_pressed():
 	t += 1
 	if t >= 3:
-		get_tree().change_scene_to_file("res://scenes/levels/test_levels/ren_test_level.tscn")
+		get_tree().change_scene_to_file("res://scenes/levels/levels/level1.tscn")
 	else:
 		$CanvasLayer/Control/ButtonOk.hide()
 		$CanvasLayer/Control/Label.text = ""
